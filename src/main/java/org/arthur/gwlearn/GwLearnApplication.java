4cf001f93d4a2bb557a09323ec1d13521c614773
ysKim <znxkznxk1030@gmail.com>
2022-01-29 21:11:41 +0530

    first commit

---
+++ src/main/java/org/arthur/gwlearn/GwLearnApplication.java
@@ -0,0 +1,13 @@
+package org.arthur.gwlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/arthur/gwlearn/GwLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arthur.gwlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
