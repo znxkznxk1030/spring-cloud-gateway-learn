1397334962a8f7e6ee3073cc21f4bb7c111533ac
ysKim <znxkznxk1030@gmail.com>
2022-01-30 13:12:18 +0530

    circuitbreaker 설정

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Building a Gateway
 
+## Using Spring Cloud CircuitBreaker
+
+```xml
+<!-- circuitbreaker -->
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-circuitbreaker-reactor-resilience4j</artifactId>
+</dependency>
+```
+
+``` java
+@Bean
+public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    return builder.routes()
+        .route(p -> p
+            .path("/get")
+            .filters(f -> f.addRequestHeader("Hello", "World"))
+            .uri("http://httpbin.org:80"))
+        .route(p -> p
+            .host("*.circuitbreaker.com")
+            .filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+            .uri("http://httpbin.org:80")).
+        build();
+}
+```
--- src/main/java/org/arthur/gwlearn/GwLearnApplication.java
+++ src/main/java/org/arthur/gwlearn/GwLearnApplication.java
@@ -2,12 +2,45 @@ package org.arthur.gwlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class GwLearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GwLearnApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GwLearnApplication.class, args);
+  }
+
+  @Bean
+  public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    return builder
+        .routes()
+        .route(p -> p.path("/get")
+            .filters(f -> f.addRequestHeader("Hello", "World"))
+            .uri("http://httpbin.org:80"))
+        .route(p -> p.host("*.circuitbreaker.com")
+            .filters(f -> f.circuitBreaker(config -> config
+                .setName("mycmd")
+                .setFallbackUri("forward:/fallback")))
+            .uri("http://httpbin.org:80"))
+        .build();
+  }
+
+  @RequestMapping("/fallback")
+  public Mono<String> fallback() {
+    return Mono.just("fallback");
+  }
+}
+
+@ConfigurationProperties
+class UriConfiguration {
 
 }
