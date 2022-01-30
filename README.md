a15720075311826d2000221acef8166f1cec9cef
ysKim <znxkznxk1030@gmail.com>
2022-01-30 13:39:28 +0530

    Testing

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Building a Gateway
 
+<https://spring.io/guides/gs/gateway/>
+
 ## Using Spring Cloud CircuitBreaker
 
 ```xml
@@ -23,5 +25,51 @@ public RouteLocator myRoutes(RouteLocatorBuilder builder) {
             .filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
             .uri("http://httpbin.org:80")).
         build();
+}
+```
+
+## Testing
+
+### Using WireMock
+
+<https://cloud.spring.io/spring-cloud-contract/2.0.x/multi/multi__spring_cloud_contract_wiremock.html>
+
+``` java
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
+public class ApplicationTest {
+
+  @Autowired
+  private WebTestClient webClient;
+
+  @Test
+  public void contextLoads() throws Exception {
+    //Stubs
+    stubFor(get(urlEqualTo("/get"))
+        .willReturn(aResponse()
+          .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+          .withHeader("Content-Type", "application/json")));
+    stubFor(get(urlEqualTo("/delay/3"))
+      .willReturn(aResponse()
+        .withBody("no fallback")
+        .withFixedDelay(3000)));
+
+    webClient
+      .get().uri("/get")
+      .exchange()
+      .expectStatus().isOk()
+      .expectBody()
+      .jsonPath("$.headers.Hello").isEqualTo("World");
+
+    webClient
+      .get().uri("/delay/3")
+      .header("Host", "www.circuitbreaker.com")
+      .exchange()
+      .expectStatus().isOk()
+      .expectBody()
+      .consumeWith(
+        response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
+  }
 }
 ```
--- src/test/java/org/arthur/gwlearn/GwLearnApplicationTests.java
+++ src/test/java/org/arthur/gwlearn/GwLearnApplicationTests.java
@@ -1,13 +1,48 @@
 package org.arthur.gwlearn;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
 class GwLearnApplicationTests {
+
+  @Autowired
+  private WebTestClient webClient;
 
 	@Test
 	void contextLoads() {
+      // Stubs
+      stubFor(get(urlEqualTo("/get"))
+          .willReturn(aResponse()
+              .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+              .withHeader("Content-Type", "application/json")));
+      stubFor(get(urlEqualTo("/delay/3"))
+          .willReturn(aResponse()
+              .withBody("no fallback")
+              .withFixedDelay(3000)));
+
+      webClient
+          .get().uri("/get")
+          .exchange()
+          .expectStatus().isOk()
+          .expectBody()
+          .jsonPath("$.headers.Hello").isEqualTo("World");
+
+      webClient
+          .get().uri("/delay/3")
+          .header("Host", "www.circuitbreaker.com")
+          .exchange()
+          .expectStatus().isOk()
+          .expectBody()
+          .consumeWith(
+              response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
 	}
 
 }
